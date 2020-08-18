77760c66369b8ee85679a35be5f77668547864e6
James Bound <mrjamesbound@outlook.com>
2020-08-18 10:16:12 -0400

    Test

---
+++ BlazorApp1/Data/Person.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FluentValidation;
+
+namespace BlazorApp1.Data
+{
+    public class Person
+    {
+        public string PersonName { get; set; }
+        public DateTime? PersonBirthDate { get; set; }
+
+    }
+
+    public class PersonValidator : AbstractValidator<Person>
+    {
+        public PersonValidator()
+        {
+            RuleFor(r => r.PersonName).NotEmpty().WithMessage("The name counld not be empty!");
+            RuleFor(r => r.PersonBirthDate).Must(BirthDateValidator).WithMessage("Error date!");
+
+        }
+
+        private bool BirthDateValidator(DateTime? date)
+        {
+            return !(date == null || (date < DateTime.Now.AddYears(-99) || date > DateTime.Now.AddYears(-18)));
+        }
+
+
+    }
+
+}
